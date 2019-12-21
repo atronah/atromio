b3c8ba9f99726d9043ab50983d436d71daef8145
atronah <atronah.ds@gmail.com>
2019-12-21 12:39:49 +0300

    ft(routes): adds new views for adding account and transfers

--- atromio/routes.py
+++ atromio/routes.py
@@ -1,3 +1,5 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('add_account', '/add_account')
+    config.add_route('add_transfer', '/add_transfer')
