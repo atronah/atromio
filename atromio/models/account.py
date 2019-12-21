737834577babff897e6afdd474e20087ed122ba3
atronah <atronah.ds@gmail.com>
2019-12-21 15:45:27 +0300

    ft(models/account): adds relations between account and transfers

--- atromio/models/account.py
+++ atromio/models/account.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Index
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
 
 from atromio.models.meta import Base
 
@@ -7,6 +9,12 @@ class Account(Base):
     __tablename__ = 'account'
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
+    outcomes = relationship('Transfer', foreign_keys='Transfer.source_account_id', backref='source')
+    incomes = relationship('Transfer', foreign_keys='Transfer.target_account_id', backref='target')
+
+    @hybrid_property
+    def transfers(self):
+        return self.incomes + self.outcomes
 
 
 Index('idx_account_name', Account.name, unique=True)
