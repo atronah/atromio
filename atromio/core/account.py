485720ab4d18acc285137c6a476929324c1dba61
atronah <atronah.ds@gmail.com>
2019-12-21 20:32:44 +0300

    ft(core/account): adds transfers correction during adding real_balance

--- atromio/core/account.py
+++ atromio/core/account.py
@@ -1,5 +1,6 @@
 import transaction
 
+from atromio.core.transfer import make_transfer
 from atromio.models import Account, RealBalance
 
 
@@ -14,8 +15,14 @@ def get_accounts(session):
     return session.query(Account).all()
 
 
-def add_real_balance(session, account_id, amount, confirmed_at):
-    real_balance = RealBalance(account_id=account_id, amount=amount, confirmed_at=confirmed_at)
+def add_real_balance(session, account_id, real_balance, confirmed_at):
+    account = session.query(Account).get(account_id)
+    account_balance = account.balance(confirmed_at)
+    if real_balance > account_balance:
+        make_transfer(session, real_balance - account_balance, confirmed_at, None, account.id)
+    elif real_balance < account_balance:
+        make_transfer(session, account_balance - real_balance, confirmed_at, account.id, None)
+    real_balance = RealBalance(account_id=account_id, amount=real_balance, confirmed_at=confirmed_at)
     session.add(real_balance)
     session.flush()
     return real_balance
